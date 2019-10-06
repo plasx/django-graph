910967922c732b49a38282d15cf15442311cdbf3
Daniel Rivera <plasx@live.com>
2019-10-06 08:57:21 -0400

    added update and delete mutations for movies

--- movies/api/schema.py
+++ movies/api/schema.py
@@ -42,3 +42,52 @@ class Query(graphene.ObjectType):
             return Movie.objects.get(title=title)
 
         return None
+
+class MovieCreateMutation(graphene.Mutation):
+    class Arguments:
+        title = graphene.String(required=True)
+        year = graphene.Int(required=True)
+
+    movie = graphene.Field(MovieType)
+
+    def mutate(self, info, title, year):
+        movie = Movie.objects.create(title=title, year=year)
+
+        return MovieCreateMutation(movie=movie)
+
+
+class MovieUpdateMutation(graphene.Mutation):
+    class Arguments:
+        title = graphene.String()
+        year = graphene.Int()
+        id = graphene.ID(required=True)
+
+    movie = graphene.Field(MovieType)
+
+    def mutate(self, info, id, title, year):
+        movie = Movie.objects.get(pk=id)
+        if title is not None:
+            movie.title = title
+        if year is not None:
+            movie.year = year
+        movie.save()
+
+        return MovieUpdateMutation(movie=movie)
+
+
+class MovieDeleteMutation(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+
+    movie = graphene.Field(MovieType)
+
+    def mutate(self, info, id):
+        movie = Movie.objects.get(pk=id)
+        movie.delete()
+
+        return MovieUpdateMutation(movie=None)
+
+class Mutation:
+    create_movie = MovieCreateMutation.Field()
+    update_movie = MovieUpdateMutation.Field()
+    delete_movie = MovieDeleteMutation.Field()
--- movies/schema.py
+++ movies/schema.py
@@ -1,7 +1,13 @@
 import graphene
 import movies.api.schema
 
+
 class Query(movies.api.schema.Query, graphene.ObjectType):
     pass
 
-schema = graphene.Schema(query=Query)
+
+class Mutation(movies.api.schema.Mutation, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
