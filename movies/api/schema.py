b34a6e180c4c6a5974f164f1f43538d5b049ab87
Daniel Rivera <plasx@live.com>
2019-10-06 08:02:53 -0400

    added directory object to queries

--- movies/api/schema.py
+++ movies/api/schema.py
@@ -1,15 +1,24 @@
 import graphene
 from graphene_django.types import DjangoObjectType
-from .models import Movie
+from .models import Movie, Director
 
 
 class MovieType(DjangoObjectType):
     class Meta:
         model = Movie
 
+    movie_age = graphene.String()
+
+    def resolve_movie_age(self, info):
+        return "Old movie" if self.year < 2000 else "New movie"
+
+class DirectorType(DjangoObjectType):
+    class Meta:
+        model = Director
 
 class Query(graphene.ObjectType):
     all_movies = graphene.List(MovieType)
+    all_directors = graphene.List(DirectorType)
     movie = graphene.Field(
         MovieType,
         id=graphene.Int(),
@@ -18,6 +27,9 @@ class Query(graphene.ObjectType):
 
     def resolve_all_movies(self, info, **kwargs):
         return Movie.objects.all()
+
+    def resolve_all_directors(self, info, **kwargs):
+        return Director.objects.all()
 
     def resolve_movie(self, info, **kwargs):
         id = kwargs.get('id')
